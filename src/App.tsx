7cb66858d3e543412d54ce7efcdc2d11ae707404
polozovaanastasia <polozova.anastasia@gmail.com>
2025-03-06 19:51:25 +0500

    add loader during fetch request.

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import AddPostForm from "./components/AddPostForm/AddPostForm";
 import PostFilter from "./components/PostFilter/PostFilter";
 import PostList from "./components/PostsList/PostList";
 import Button from "./components/UI/Button/Button";
+import Loader from "./components/UI/Loader/Loader";
 import Modal from "./components/UI/Modal/Modal";
 import { usePosts } from "./hooks/usePosts";
 
@@ -32,19 +33,25 @@ function App() {
     const [filter, setFilter] = useState<FilterType>({ sort: "", query: "" });
     const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
 
+    const [isLoading, setIsLoading] = useState(true);
+
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
+    async function fetchPosts() {
+        await axios
+            .get("https://jsonplaceholder.typicode.com/posts")
+            .then((response) => {
+                setPosts(response.data);
+            })
+            .catch((error) => {
+                new Error(error);
+            })
+            .finally(() => {
+                setIsLoading(false);
+            });
+    }
+
     useEffect(() => {
-        async function fetchPosts() {
-            await axios
-                .get("https://jsonplaceholder.typicode.com/posts")
-                .then((response) => {
-                    setPosts(response.data);
-                })
-                .catch((error) => {
-                    new Error(error);
-                });
-        }
         fetchPosts();
     }, []);
 
@@ -56,17 +63,21 @@ function App() {
         setPosts((lastPosts) => [...lastPosts, newPost]);
         setIsModalOpen(false);
     }
-
     return (
         <div className="app">
             <Button onClick={() => setIsModalOpen(true)}>Добавить пост</Button>
             <br />
             <PostFilter filter={filter} setFilter={setFilter} />
-            <PostList
-                title={"Technology list"}
-                posts={sortedAndSearchedPosts}
-                onRemovePostHandler={onRemovePostHandler}
-            />
+
+            {isLoading ? (
+                <Loader />
+            ) : (
+                <PostList
+                    title={"Technology list"}
+                    posts={sortedAndSearchedPosts}
+                    onRemovePostHandler={onRemovePostHandler}
+                />
+            )}
             <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
                 <>
                     <h2>Новый пост:</h2>
---
+++ src/components/UI/Loader/Loader.tsx
@@ -0,0 +1,43 @@
+import styles from "./Loader.module.css";
+
+type LoaderPropsType = {
+    size?: number;
+    color?: string;
+};
+
+function Loader({ size = 24, color = "rgb(7, 153, 95)" }: LoaderPropsType) {
+    console.log("Loader rendered");
+    return (
+        <svg
+            width={size}
+            height={size}
+            viewBox="0 0 50 50"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className={styles["loader"]}
+        >
+            <circle
+                cx="25"
+                cy="25"
+                r="20"
+                stroke="rgba(0, 0, 0, 0.1)" // Серый цвет (фон)
+                strokeWidth="4"
+                fill="none"
+            />
+            <circle
+                cx="25"
+                cy="25"
+                r="20"
+                stroke={color} // Цвет активного сегмента
+                strokeWidth="4"
+                strokeLinecap="round"
+                fill="none"
+                strokeDasharray="125" // Длина окружности (примерно 2 * π * r)
+                strokeDashoffset="100" // Смещение (делает кусочек)
+                className={styles["spinner"]}
+            />
+        </svg>
+    );
+}
+
+export default Loader;
